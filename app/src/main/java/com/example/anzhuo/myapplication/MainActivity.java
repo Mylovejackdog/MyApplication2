2ebf1888fbe1f2e534306015d9df3ff8b5cbe631
css <c243486829@vip.qq.com>
2016-09-06 15:35:37 +0800

    第一个APP

---
+++ app/src/main/java/com/example/anzhuo/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.anzhuo.myapplication;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}

e7ec3398565bf9b4383e4606fee4b1a07901fce1
css <c243486829@vip.qq.com>
2016-09-06 18:21:30 +0800

    第一个APP

--- app/src/main/java/com/example/anzhuo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anzhuo/myapplication/MainActivity.java
@@ -1,18 +1,9 @@
 package com.example.anzhuo.myapplication;
 
-import android.os.Handler;
-import android.os.Message;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
 
 }

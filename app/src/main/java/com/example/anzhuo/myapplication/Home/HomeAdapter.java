3de8c6a435b8a76188919f4c4f0712c0ed022091
css <c243486829@vip.qq.com>
2016-09-09 16:59:42 +0800

    11

---
+++ app/src/main/java/com/example/anzhuo/myapplication/Home/HomeAdapter.java
@@ -0,0 +1,28 @@
+package com.example.anzhuo.myapplication.Home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by anzhuo on 2016/9/9.
+ */
+public class HomeAdapter extends FragmentPagerAdapter {
+    List<Fragment>list;
+    public HomeAdapter(FragmentManager fm, List<Fragment>list) {
+        super(fm);
+        this.list=list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return list==null?0:list.size();
+    }
+}

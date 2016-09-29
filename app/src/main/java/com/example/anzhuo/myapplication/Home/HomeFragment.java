c3f37f5c99876553668d744c6e872d9475f2bc10
css <c243486829@vip.qq.com>
2016-09-29 17:27:04 +0800

    首页

--- app/src/main/java/com/example/anzhuo/myapplication/Home/HomeFragment.java
+++ app/src/main/java/com/example/anzhuo/myapplication/Home/HomeFragment.java
@@ -29,53 +29,54 @@ public class HomeFragment extends Fragment {
     ViewPager vp_main;
     HomeAdapter homeAdapter;
     RadioGroup rg_head;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.oldcar_home_layout,null);
+        View view = inflater.inflate(R.layout.oldcar_home_layout, null);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       vp_main= (ViewPager) view.findViewById(R.id.vp_main);
-        rg_head= (RadioGroup) view.findViewById(R.id.rg_head);
-        list=new ArrayList<>();
-        recommendActivity=new RecommendFragment();
-        textActivity=new TextFragment();
-        pictureActivity=new PictureFragment();
-        gifActivity=new GifFragment();
-        videoActivity=new VideoFragment();
+        vp_main = (ViewPager) view.findViewById(R.id.vp_main);
+        rg_head = (RadioGroup) view.findViewById(R.id.rg_head);
+        list = new ArrayList<>();
+        recommendActivity = new RecommendFragment();
+        textActivity = new TextFragment();
+        pictureActivity = new PictureFragment();
+        gifActivity = new GifFragment();
+        videoActivity = new VideoFragment();
         list.add(recommendActivity);
         list.add(textActivity);
         list.add(pictureActivity);
         list.add(gifActivity);
         list.add(videoActivity);
-        homeAdapter=new HomeAdapter(getFragmentManager(),list);
+        homeAdapter = new HomeAdapter(getFragmentManager(), list);
         vp_main.setAdapter(homeAdapter);
-     rg_head.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-         @Override
-         public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                 switch (checkedId){
-                     case R.id.rb_recommend:
-                         vp_main.setCurrentItem(0);
-                         break;
-                     case R.id.rb_text:
-                         vp_main.setCurrentItem(1);
-                         break;
-                     case R.id.rb_pic:
-                         vp_main.setCurrentItem(2);
-                         break;
-                     case R.id.rb_gif:
-                         vp_main.setCurrentItem(3);
-                         break;
-                     case R.id.rb_video:
-                         vp_main.setCurrentItem(4);
-                         break;
-                 }
-         }
-     });
+        rg_head.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_recommend:
+                        vp_main.setCurrentItem(0);
+                        break;
+                    case R.id.rb_text:
+                        vp_main.setCurrentItem(1);
+                        break;
+                    case R.id.rb_pic:
+                        vp_main.setCurrentItem(2);
+                        break;
+                    case R.id.rb_gif:
+                        vp_main.setCurrentItem(3);
+                        break;
+                    case R.id.rb_video:
+                        vp_main.setCurrentItem(4);
+                        break;
+                }
+            }
+        });
 
         vp_main.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -86,9 +87,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onPageSelected(int position) {
 
-                switch (position){
+                switch (position) {
                     case 0:
-                       rg_head.check(R.id.rb_recommend);
+                        rg_head.check(R.id.rb_recommend);
                         break;
                     case 1:
                         rg_head.check(R.id.rb_text);
